4714d3f7fb8fcccc4fb65c9f65ceff5217f7bcfb
Salamek <noemail@no.email>
2026-06-19 14:38:54 +0200

    Odebrání: DÚ - IKT, odevzdávání učebnic, Hrajeme si i hlavou; Upravení: exkurze v Josefově, sportovní den

--- data.js
+++ data.js
@@ -1,26 +1,8 @@
 var data = [
   {
-    "name": "Domácí úkol - IKT",
-    "date": "17.06.2026",
-    "task": "Nástěnka na umimeto.org",
-    "solution": ""
-  },
-  {
-    "name": "Odevzdávání učebnic",
-    "date": "18.06.2026",
-    "task": "Odevzdávání učebnic 17. 6. - 18. 6. 7:30 - 7:55 do bilogické laboratoře. (musí se přinést i lístek se seznamem)<br><b>seznam je v řešení</b>",
-    "solution": "<b>Učebnice, co si necháváme:</b><br>Pravidla českého pravopisu<br>Tabulky pro ZŠ<br>Školní atlas světa<br><br><b>Učebnice, co se odevzdávají:</b><br>Český jazyk 7<br>Čítanka 7<br>Zeměpis světa 2<br>Zeměpis světa 3<br>Fyzika 7<br>Základy chemie 1<br>Přírodopis 7<br>Dějepis 7 - středověk<br>Středověk - atlas<br>Matematika 7<br>Pracovní sešit z matematiky 7 (Odvárko)<br>Výchova k občanství 7"
-  },
-  {
-    "name": "Akce - Hrajeme si i hlavou",
-    "date": "19.06.2026",
-    "task": "Termín: Pátek 19. 6. 2026<br>Sraz: v 7:50 na vlakovém nádraží v Jaroměři<br>Odjezd: vlakem v 8:03<br>Jízdné: jízdenku na vlak do Hradce Králové a zpět si žáci zakoupí samostatně<br>Předpokládaný návrat: odjezd z HK vlakem ve 12:35, příjezd do Jaroměře v 12:58<br>Ukončení exkurze: na nádraží v Jaroměři, zvažte odhlášení oběda ve školní jídelně.",
-    "solution": ""
-  },
-  {
     "name": "Akce - Pevnost Josefov",
     "date": "22.06.2026",
-    "task": "Exkurze do Josefova - program ideální pevnostní město<br>sraz ve škole v 8:00<br>vstupné je už zaplacené",
+    "task": "<b>Exkurze do Josefova</b><br>Sraz v 8:00 u botníku<br>Prohlídka začne v 9:00 a bude trvat 1,5 hodiny<br>Vstupné je už vybráno (110 Kč)<br>Vezměte si s sebou pití, svačinu a nějakou mikinu (bude chladno)<br>Konec akce bude mezi 12:00 a 13:00",
     "solution": ""
   },
   {
@@ -36,9 +18,9 @@ var data = [
     "solution": ""
   },
   {
-    "name": "Akce - sportovní den",
+    "name": "Akce - sportovní den (míčové hry)",
     "date": "25.06.2026",
-    "task": "míčové hry, přihlašte se u kabinetu tělesné výchovy na nástěnce",
+    "task": "týmy najdete na třídní skupině",
     "solution": ""
   },
   {
